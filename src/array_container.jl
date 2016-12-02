6c6cb3a722f814cd7c2b3d40e4996694a194554c
Ben J. Ward <axolotlfan9250@gmail.com>
2016-12-02 15:50:41 +0000

    Added some docstrings

--- src/array_container.jl
+++ src/array_container.jl
@@ -60,11 +60,10 @@ end
     return present
 end
 
+"Check whether `value` is present in the `ArrayContainer` `container`."
 @inline function contains{T<:Unsigned}(container::ArrayContainer, value::T)
     return !isempty(searchsorted(container.arr, value))
 end
-
-
 
 @inline function isequal(a::ArrayContainer, b::ArrayContainer)
     return (cardinality(a) == cardinality(b)) && (a.arr == b.arr)
