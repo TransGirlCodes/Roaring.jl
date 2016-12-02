ca5bbdb963c184727e7af81688710502dccacd16
Ben J. Ward <axolotlfan9250@gmail.com>
2016-12-02 17:22:20 +0000

    Added select function

--- src/array_container/type.jl
+++ src/array_container/type.jl
@@ -25,3 +25,38 @@ end
 
 "Test that the `ArrayContainer` `x` is empty."
 isempty(x::ArrayContainer) = cardinality(x) == 0
+
+#=
+/**
+ * If the element of given rank is in this container, supposing that the first
+ * element has rank start_rank, then the function returns true and sets element
+ * accordingly.
+ * Otherwise, it returns false and update start_rank.
+ */
+static inline bool array_container_select(const array_container_t *container,
+                                          uint32_t *start_rank, uint32_t rank,
+                                          uint32_t *element) {
+    int card = array_container_cardinality(container);
+    if (*start_rank + card <= rank) {
+        *start_rank += card;
+        return false;
+    } else {
+        *element = container->array[rank - *start_rank];
+        return true;
+    }
+}
+=#
+"""
+If the element of given rank is in this container, supposing that the first
+element has rank start_rank, then the function returns true and sets element
+accordingly.
+Otherwise, it returns false and update start_rank.
+"""
+@inline function select(x::ArrayContainer, startrank::UInt32, rank::UInt32)
+    c = cardinality(x)
+    if (startrank + c) <= rank
+        return (false, startrank + c, 0x00000000)
+    else
+        return (true, startrank, x.arr[(rank - startrank) + 1])
+    end
+end
