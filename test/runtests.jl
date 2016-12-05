234aee4427bd60bbd0687698b3a1d3a642e4d28e
Ben J. Ward <axolotlfan9250@gmail.com>
2016-12-05 15:36:36 +0000

    Added first few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,22 @@
+module TestRoaring
 
+using Base.Test
 
+using Roaring
 
 @testset "Containers" begin
+
     @testset "ArrayContainer" begin
 
+        # Construction.
+        @test Roaring.cardinality(Roaring.ArrayContainer()) == 0
+        @test Roaring.cardinality(Roaring.ArrayContainer()) == Roaring.cardinality(Roaring.ArrayContainer())
+        @test isempty(Roaring.ArrayContainer())
+
+        
+
     end
+
 end
+
+end # module
